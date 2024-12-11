7995d8c69ef4ab23de57c7b9192a063b0496a242
maxwendell1 <70673895+maxwendell1@users.noreply.github.com>
2024-12-11 15:14:18 -0300

    Commit inicial projetoUM

---
+++ src/main/java/acc/br/demo/ProjetoUmApplication.java
@@ -0,0 +1,13 @@
+package acc.br.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUmApplication.class, args);
+	}
+
+}
